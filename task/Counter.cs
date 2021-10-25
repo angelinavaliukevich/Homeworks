cdbf177f56aadffe86547a58b77ebd881df5bf2d
ангелина валюкевич <angelinka.valyukevich@mail.ru>
2021-10-25 21:56:53 +0300

    дз task

---
+++ task/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Counter
+    {
+        private static readonly int[] array;
+        private static long  totalCount;
+
+        public  Counter(int [] array)
+        {
+           // this.array = array;
+        }
+
+        private static void InternalCalculaate(object obj)
+        {
+            int[] partOfArray = obj as int [];
+            foreach (var item in partOfArray)
+            {
+                Interlocked.Add(ref totalCount, item);
+            }
+        }
+        public static async Task Calculate(int[] array)
+        {
+            for ( int i =0; i< 10; i++)
+            {
+               int[] partArray = array.Skip(i*100_000).Take(100_000).ToArray();
+               await Task.Run(() => InternalCalculaate(partArray));
+                //threads[i] = new Thread(InternalCalculaate);
+                //threads[i].Start(partArray);
+            }
+        }
+        public static async void CalculateOut(int[] setarray) {
+            //array = new setarray;
+            await Calculate(setarray);
+            Console.WriteLine(totalCount);
+        }
+
+    }
+}
---
+++ task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int[] array = Enumerable.Range(1, 1000_000).ToArray();
+            long count = 0;
+            foreach (var item in array)
+            {
+                count += item;
+            }
+            Console.WriteLine("1)"+count);
+
+            Counter.CalculateOut(array);
+            Console.ReadKey();
+            Console.WriteLine($"Время загрузки: {sw.Elapsed}");
+           
+        }
+    }
+}
