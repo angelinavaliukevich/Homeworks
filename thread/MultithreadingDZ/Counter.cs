35d79c80c62a0ef97d7b3cda809866c24ca3f865
ангелина валюкевич <angelinka.valyukevich@mail.ru>
2021-10-25 21:45:03 +0300

    дз потоки

---
+++ thread/MultithreadingDZ/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingDZ
+{
+    internal class Counter
+    {
+        private readonly int[] array;
+        private long  totalCount;
+
+        public  Counter(int [] array)
+        {
+            this.array = array;
+        }
+        private void InternalCalculaate(object obj)
+        {
+            int[] partOfArray = obj as int [];
+            foreach (var item in partOfArray)
+            {
+                Interlocked.Add(ref totalCount, item);
+            }
+        }
+        public long  Calculate()
+        {
+            Thread[] threads = new Thread[10];
+            for ( int i =0; i< 10; i++)
+            {
+               int[] partArray = array.Skip(i*100_000).Take(100_000).ToArray();
+                threads[i] = new Thread(InternalCalculaate);
+                threads[i].Start(partArray);
+                
+            }
+             foreach (var item in threads)
+            {
+                item.Join();
+            }
+            return totalCount;
+        }
+    }
+}
---
+++ thread/MultithreadingDZ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace MultithreadingDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //    Timer timer = new Timer(() => Console.WriteLine("///"), 2);
+
+            //    timer.Start();
+            //    Thread.Sleep(5000);
+            //    timer.Stop();
+            int[] array = Enumerable.Range(1, 1000_000).ToArray();
+            long count  = 0;
+            foreach (var item in array)
+            {
+                count += item;
+             }
+            Console.WriteLine(count);
+            Counter counter = new Counter(array);
+            Console.WriteLine(counter.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
